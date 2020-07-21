36c9dcfb2142b0148d236c501988c4c5e95195e6
SLawpie <slawpie@hotmail.com>
2020-07-21 21:32:04 +0200

    as 4x on-off

--- index.js
+++ index.js
@@ -14,15 +14,17 @@ module.exports = function (homebridge)
     homebridge.registerAccessory("homebridge-wlightbox-mono", "WLightBoxMono", WLightBoxSwitch)
 }
 
+class WLightBoxSwitch {
 
-function WLightBoxSwitch(log, config) {
-
+    constructor (log, config) {
         this.services = [];
+        this.service
         this.log = log;
         this.name = config.name;
         this.channels = config.channels || 4;
         this.names = config.names || ['Switch 0', 'Switch 1', 'Switch 2', 'Switch 3'];
         this.ip = config.ip;
+        this.updateInterval = config.updateInterval || 60000;
         this.current_status = [0,0,0,0];
         this.status_callbacks = new Array();
         this.current_status_time = null;
@@ -32,16 +34,89 @@ function WLightBoxSwitch(log, config) {
         for (let i = 0; i < this.channels; i++) {
             const switchName = (this.name).concat(' ', (this.names[i]));
             const subtype = "switch" + i;
-            this.services[i] = new Service.Switch(switchName, subtype);
-            this.log.debug(`Register switch: ${switchName} / ${subtype}`);
-        }
-        //this._service = new Service.Switch((this.name).concat(' ', (this.names[0])));
-}
-  
+            
+            this.services[i] = new Service.Lightbulb(switchName, subtype);
 
-WLightBoxSwitch.prototype.getServices = function() {
-    return this.services;
+            this.services[i].getCharacteristic(Characteristic.On)
+                 .on('get', this.statusLampGet.bind(this, i))
+                 .on('set', this.statusLampSet.bind(this, i));
+            this.services.push(this.switch1Service);
+            
+            this.log.debug(`Register LED strip: ${switchName} / ${subtype}`);
+        };
+
+        this.updateStatus(true);
+    }
+
+    getServices() {
+        return this.services;
+    }
+
+    statusLampGet(number, callback) {
+
+        this.log.debug(`Number GET : ${number}`);
+
+        this.statusGet(false, (error) => {
+            if (error) {
+                callback(error);
+                return;
+            }
+
+        });
+        callback(null, this.current_status[number] > 0);
+        this.log.debug(`Triggered GET : ${this.current_status[number]}`);
+    }
+
+    statusLampSet(number, value, callback) {
+
+        this.log.debug(`Triggered SET On: ${value} for ${number}`);
+
+        if (value)
+            this.current_status[number] = 255;
+        else
+            this.current_status[number] = 0;
+
+        callback(null);
+    }
+
+    statusGet(forced, callback) {
+
+        if (forced) {
+            const dummy = "ff00ff00";
+
+            for (let index = 0; index < 8; index++) {
+                var str = dummy.substr(index, 2);
+                this.log.debug(`${Math.floor(index/2)} - substr [${index}]: ${str} -> ${dummy}`);
+                this.current_status[Math.floor(index/2)] = parseInt("0x" + str);
+                index++;
+
+            }
+        }
+
+    }
+
+    updateStatus(forced = flase) {
+        this.log.debug('Updating switch status');
+        this.statusGet(forced, (err) => {
+            if (err) {
+                this.log.debug(`Updating ERROR: ${err}`);
+                return;
+            }
+        });
+
+        this.log.debug('Updating characteristics');
+
+        for (let index = 0; index < this.channels; index++) {
+            this.services[index].updateCharacteristic(Characteristic.On,  this.current_status[index] > 0);
+        };
+       
+    }
+
+
+
 }
+
+
 
     // sendJSONRequest (url, method = 'GET', payload = null) {
     //     return new Promise((resolve, reject) => {
